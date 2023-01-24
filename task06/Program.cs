67a939e4356dc410462c6729d481c41708ce7c89
Ganjinka <ganjinka.gm@gmail.com>
2023-01-24 22:43:36 +0500

    Added task04

---
+++ task06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vvedite chislo");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int ostatok = numberN % 2; 
+if (ostatok == 0) {
+    Console.WriteLine("Da");
+} else {
+    Console.WriteLine("Net");
+}
