3bde9c44093267102ef6f6122fa5638e31abc606
Ganjinka <ganjinka.gm@gmail.com>
2023-01-24 22:16:13 +0500

    Added task02

---
+++ task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Vvedite chislo A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite chislo B:");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B) {
+    Console.WriteLine( A );
+} else if (B > A) {
+    Console.WriteLine( B );
+} else {
+    Console.WriteLine(A + " = " + B);
+}
