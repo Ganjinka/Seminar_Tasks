979d5f1b79bff2628561daf9b1fa46aeb5243894
Ganjinka <ganjinka.gm@gmail.com>
2023-01-24 22:53:04 +0500

    Added task08

---
+++ task08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vvedite chislo");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Chetnie chisla do " + numberN);
+for (int i = 1; i <= numberN; i++) {
+ if ((i % 2) == 0) {
+  Console.WriteLine(i);  
+ }
+}
