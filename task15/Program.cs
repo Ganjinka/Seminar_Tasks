c137900da570372e5d836d88e987a2df9b869a03
Ganjinka <ganjinka.gm@gmail.com>
2023-01-30 22:12:43 +0500

    Added task15

---
+++ task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Vvedite chislo");
+int daynNumber = Convert.ToInt32(Console.ReadLine());
+if (daynNumber <= 5 && daynNumber >= 1) {
+    Console.WriteLine("No");
+} else if (daynNumber == 6 || daynNumber == 7) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("Wrong dayNumber");
+}
