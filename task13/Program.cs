12cfce3fd80cad2f97a98805725e9fb019320036
Ganjinka <ganjinka.gm@gmail.com>
2023-01-30 21:51:23 +0500

    Added task13

---
+++ task13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vvedite chislo");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberN < 100) {
+  Console.WriteLine("Tretey sifri net");  
+ } else {
+    Console.WriteLine(numberN % 10);
+ }
+ 
